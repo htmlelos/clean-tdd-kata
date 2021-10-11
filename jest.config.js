bb48e654b0280c49c487f9d76baf93c8974e8390
Sergio Lucero <sergiolucero73@gmail.com>
2021-10-11 17:01:06 -0300

    chores: config-jest

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,14 @@ module.exports = {
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
   },
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
 }
--- src/test/foo.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from '../foo';
-
-test('basic', () => {
-    expect(sum()).toBe(0);
-})
-
-test('basic again', () => {
-    expect(sum(1, 2, 4)).toBe(7);
-})
